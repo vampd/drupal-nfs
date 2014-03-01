8ad99df33bedf5ed68ba833e73d97f2ee90933c6
Alex Knoll <arknoll@gmail.com>
2014-03-01 13:43:55 -0700

    Add nfs to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          "Apache 2.0"
 description      "Installs/configures nfs"
 version          "0.1.0"
 recipe           "drupal-nfs::default", "Installs/configures nfs"
+
+depends "nfs"
